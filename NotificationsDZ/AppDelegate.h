402065f38b16c75009d0eda720365c56c9eb1588
Elerman <elerman@air-elerman.dlink>
2016-05-03 16:43:08 +0300

    Initial Commit

---
+++ NotificationsDZ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationsDZ
+//
+//  Created by Elerman on 03.05.16.
+//  Copyright © 2016 Elerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotificationsDZ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotificationsDZ
+//
+//  Created by Elerman on 03.05.16.
+//  Copyright © 2016 Elerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
